e20e7263215a02bacb3454d3854bf2db8bd1c3fc
Nallamekala.Harshanvitha <Nallamekala.harshanvitha@accionlabs.com>
2026-09-15 16:01:03 +0530

    first commit

---
+++ memory-game/src/App.jsx
@@ -0,0 +1,51 @@
+import { GameHeader } from './components/GameHeader';
+import { Card } from './components/Card';
+import {WinMessage} from './components/WinMessage';
+import { useGameLogic } from './hooks/useGameLogic';
+
+const cardValues = [
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🍑",
+  "🍒",
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🍑",
+  "🍒",
+];
+
+function App() {
+  const {
+    cards,
+    score,
+    moves,
+    handleCardClick,
+    initializeGame,
+    isGameComplete,
+  } = useGameLogic(cardValues);
+
+
+ return (
+ <div className="App">
+  <GameHeader score={score} moves={moves} onReset={initializeGame} />
+
+  {isGameComplete && <WinMessage moves={moves} />}
+
+ <div className="cards-grid">
+  {cards.map((card) => (
+    <Card card={card} onClick={handleCardClick} />
+   ))}
+  </div>
+ </div>
+ );
+}
+
+export default App;
